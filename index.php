b98d1a406555339773f2f7cf698c5922855a3551
Bao-Tien <qwe1593574268@gmail.com>
2024-02-28 15:05:26 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php require_once('./conn.php');?>
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="有機蘆筍 有機洋蔥" >
+    <title>酷媽媽安心農場</title>
+    <link rel="shortcut icon" href="./icon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <header class="header">
+      <div class="header__top">
+        <div class="header__top__inner">
+          <div class= description>酷媽媽安心農場 有機蘆筍 | 有機洋蔥 | 農家自產自銷</div>
+          <ul class="header__nav" >
+            <li class="header__nav__member">
+              <a href="./login.php">登入 / 註冊</a></li>
+            <li class="header__nav__cart">
+              <a href="./cart.php">購物車</a></li>
+          </ul>
+          </div>
+        <nav class="nav__bar">
+          <div class="logo">
+            <a href="./index.php" title="回首頁" alt="酷媽媽安心農場">
+              <img src="./logo.png" />
+            </a>
+          </div>
+          <ul class="nav__bar__list">
+            <li>
+              <a href="./shop.php">買菜去</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/pages/%E9%85%B7%E5%AA%BD%E5%AA%BD%E5%AE%89%E5%BF%83%E8%BE%B2%E5%A0%B4/545956475565142">FB粉絲專頁</a>
+            </li>
+            <li>
+              <a href="./board.php">留言板</a>
+            </li>
+            <li>
+              <a href="./about.php">關於我們</a>
+            </li>
+            <li>
+              <a href="./contact.php">聯絡我們</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </body>
+</html>
